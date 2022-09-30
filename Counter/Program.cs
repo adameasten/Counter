e7731a9182a34583a808acab26dabdbaf7f32496
Adam <adaost0110@hotmail.com>
2022-09-30 21:33:43 +0200

    First commit, added git repo and a counter

---
+++ Counter/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Counter
+{
+    internal class Program
+    {
+        static async void Main(string[] args)
+        {
+            var counter = 0;
+            while (true)
+            {
+                Console.WriteLine($"Counting.. {counter}!");
+                counter++;
+
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
